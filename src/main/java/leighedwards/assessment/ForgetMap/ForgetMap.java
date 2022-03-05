cd5511dd432498d405416eebcd6c3ecdfaa8dc44
Leigh Edwards <luton66@gmail.com>
2022-03-05 19:19:32 +0000

    Creating skeleton class and methods to generate tests

---
+++ src/main/java/leighedwards/assessment/ForgetMap/ForgetMap.java
@@ -0,0 +1,13 @@
+package leighedwards.assessment.ForgetMap;
+
+public class ForgetMap<K, V> {
+    public ForgetMap(int maxAssociations) {
+    }
+
+    public void add(V value) {
+    }
+
+    public V find(K key) {
+        return null;
+    }
+}
---
+++ src/test/java/leighedwards/assessment/ForgetMap/ForgetMapTest.java
@@ -0,0 +1,7 @@
+package leighedwards.assessment.ForgetMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ForgetMapTest {
+
+}
