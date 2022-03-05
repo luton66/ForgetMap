5bdedb49f1c59cba6fced851dd153537f6697c14
Leigh Edwards <luton66@gmail.com>
2022-03-05 20:05:40 +0000

    addition of README file

---
+++ README.md
@@ -0,0 +1,13 @@
+#Design
+
+The current plan for my implementation of this assessment is the following...
+
+The "Forgetting Map" will be a HashMap that will contain the key provided by the user, and a seperate object that will contain the following information
+
+- The 'content' passed in by the user
+- A time that the element was created in the map (this can be used to assist in deciding which element to remove in the event of a tie-breaker)
+- A count element that will represent the number of times the given element has been called by the find method.
+
+#Plan
+
+I have created skeleton classes that return a null class, and will use these to implement the tests that will confirm the desired outcome of the assessment.
--- src/main/java/leighedwards/assessment/ForgetMap/ForgetMap.java
+++ src/main/java/leighedwards/assessment/ForgetMap/ForgetMap.java
@@ -1,6 +1,12 @@
 package leighedwards.assessment.ForgetMap;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
+
 public class ForgetMap<K, V> {
+
+    HashMap<K, ContentMap<V>> cache = new HashMap<>();
+
     public ForgetMap(int maxAssociations) {
     }
 
@@ -9,5 +15,27 @@ public class ForgetMap<K, V> {
 
     public V find(K key) {
         return null;
+    }
+
+    private class ContentMap<V> {
+        private V contentValue;
+        private LocalDateTime timeAdded;
+        private int usageCount;
+
+        public V getContentValue() {
+            return contentValue;
+        }
+
+        public LocalDateTime getTimeAdded() {
+            return timeAdded;
+        }
+
+        public int getUsageCount() {
+            return usageCount;
+        }
+
+        public void incrementUsage(int usageCount) {
+            // Thread-safe increment of content usage.
+        }
     }
 }
