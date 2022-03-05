d453ba89522d9027cffc747071faee13b60e9dce
Leigh Edwards <luton66@gmail.com>
2022-03-05 19:10:50 +0000

    Initial Commit

---
+++ src/main/java/leighedwards/assessment/ForgetMap/ForgetMapApplication.java
@@ -0,0 +1,13 @@
+package leighedwards.assessment.ForgetMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForgetMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForgetMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/leighedwards/assessment/ForgetMap/ForgetMapApplicationTests.java
@@ -0,0 +1,13 @@
+package leighedwards.assessment.ForgetMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForgetMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
