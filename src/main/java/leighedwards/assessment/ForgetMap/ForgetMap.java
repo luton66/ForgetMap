8ecd9f150cac5e8c3257c5d7b35016917ada8ccd
Leigh Edwards <luton66@gmail.com>
2022-03-06 14:40:31 +0000

    fleshed out test content to work against and added methods for easy testing

--- src/main/java/leighedwards/assessment/ForgetMap/ForgetMap.java
+++ src/main/java/leighedwards/assessment/ForgetMap/ForgetMap.java
@@ -1,16 +1,40 @@
 package leighedwards.assessment.ForgetMap;
 
+import jdk.jshell.spi.ExecutionControl;
+
 import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Logger;
 
 public class ForgetMap<K, V> {
 
-    HashMap<K, ContentMap<V>> cache = new HashMap<>();
+    private static final Logger LOGGER = Logger.getLogger(ForgetMap.class.getName());
 
-    public ForgetMap(int maxAssociations) {
+    private ConcurrentHashMap<K, ContentMap<V>> cache = new ConcurrentHashMap<>();
+    private long maxAssociations;
+
+    public ForgetMap(long maxAssociations) {
+        this.maxAssociations = maxAssociations;
     }
 
-    public void add(V value) {
+    public synchronized void add(K k, V v) {
+        if (cache.containsKey(k)) {
+            LOGGER.info(String.format("Key: %s already found in ForgetMap, will override", k));
+        }
+    }
+
+    public synchronized long size() {
+        return cache.size();
+    }
+
+    public synchronized Set<K> keySet() {
+        return cache.keySet();
+    }
+
+    public synchronized long usageByKey(K k) {
+        return -1;
     }
 
     public V find(K key) {
@@ -20,7 +44,7 @@ public class ForgetMap<K, V> {
     private class ContentMap<V> {
         private V contentValue;
         private LocalDateTime timeAdded;
-        private int usageCount;
+        private long usageCount;
 
         public V getContentValue() {
             return contentValue;
@@ -30,7 +54,7 @@ public class ForgetMap<K, V> {
             return timeAdded;
         }
 
-        public int getUsageCount() {
+        public long getUsageCount() {
             return usageCount;
         }
 
--- src/test/java/leighedwards/assessment/ForgetMap/ForgetMapTest.java
+++ src/test/java/leighedwards/assessment/ForgetMap/ForgetMapTest.java
@@ -1,9 +1,9 @@
 package leighedwards.assessment.ForgetMap;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.convert.DataSizeUnit;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,30 +14,82 @@ class ForgetMapTest {
     @Test
     @DisplayName("Test that an element can be added and retrieved from map")
     public void confirmAddElement() {
-        assert(false);
+        forgetMap = new ForgetMap<String, String>(1);
+
+        forgetMap.add("test1", "test1 value");
+
+        assertEquals(forgetMap.find("test1"), "test1 value");
     }
 
     @Test
     @DisplayName("Test that the map will not exceed the max number of associations")
     public void confirmMaxAssociations() {
-        assert(false);
+        forgetMap = new ForgetMap<String, String>(3);
+        forgetMap.add("test1", "test value");
+        forgetMap.add("test2", "test value");
+        forgetMap.add("test3", "test value");
+        forgetMap.add("test4", "test value");
+
+        assertEquals(forgetMap.size(), 3);
     }
 
     @Test
     @DisplayName("Test that the map is successfuly removing lowest usage values")
     public void confirmRemovalOfLowestUsageValues() {
-        assert(false);
+        //create map and add elements
+        forgetMap = new ForgetMap<String, String>(3);
+        forgetMap.add("test1", "test value");
+        forgetMap.add("test2", "test value");
+        forgetMap.add("test3", "test value");
+
+        forgetMap.find("test1");
+        forgetMap.find("test1");
+
+        forgetMap.find("test2");
+        forgetMap.find("test2");
+
+        forgetMap.find("test3");
+
+        forgetMap.add("test4", "test value");
+
+        assertFalse(forgetMap.keySet().contains("test3"));
     }
 
     @Test
     @DisplayName("Test that the map is removing a value in event of tie breaker")
     public void confirmRemovalWithTieBreaker() {
-        assert(false);
+        forgetMap = new ForgetMap<String, String>(2);
+        forgetMap.add("test1", "test value");
+        forgetMap.add("test2", "test value");
+
+        forgetMap.find("test1");
+        forgetMap.find("test1");
+
+        forgetMap.find("test2");
+        forgetMap.find("test2");
+
+        forgetMap.add("test3", "test value");
+
+        assertEquals(forgetMap.size(), 2);
     }
 
     @Test
     @DisplayName("Confirm usage count is correct after asynchronous calls to map")
     public void confirmAsynchronousUsageCount() {
-        assert(false);
+        forgetMap = new ForgetMap<String, String>(2);
+        forgetMap.add("test1", "test value");
+
+        int numberOfThreads = 10;
+        ExecutorService service = Executors.newFixedThreadPool(numberOfThreads);
+
+        // 100 calls to be made to find("test1") over 10 threads
+        for (int i = 0; i < 100; i++) {
+            service.submit(() -> {
+                forgetMap.find("test1");
+                // System.out.println(Thread.currentThread().getName());
+            });
+        }
+
+        assertEquals(forgetMap.usageByKey("test1"), 100);
     }
 }
